4cecaf7af72371c619511e3069bafadd86e8bf5b
Sam Skeen <sskeen@sskeendevelopment.onmicrosoft.com>
2015-09-04 11:07:09 -0600

    Completed main assignment

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,22 +1,39 @@
 var app = angular.module('chatroom');
 
 app.controller('mainCtrl', function($scope, parseService){
+  var vm = this;
   //In your controller you'll have a getParseData function and a postData function, but should be placed on $scope.
 
   //The getParseData function will call the getData method on the parseService object. You'll then save the result of that request to 
   //your controllers $scope as messages ($scope.messages)
-
-
-
+  function applyRemoteData(newMessages) {
+    vm.messages = newMessages.results;
+  }
+  vm.getParseData = function() {
+    parseService.getData().then(function(response) {
+      applyRemoteData(response);
+    });
+  };
+  vm.getParseData();
+  
   //The postData function will take whatever the user typed in (hint: look at the html and see what ng-model correlates to on the input box),
   //pass that text to the postData method on the parseService object which will then post it to the parse backend.
-
-
-
-
+  vm.postData = function() { 
+    parseService.postData(vm.message).then(vm.getParseData, function(errorMessage) {
+      console.warn(errorMessage);
+    });
+    
+    vm.message = "";
+  };
+  
+  vm.test = function(data) {
+    console.log(data);
+  };
+  
+  
   //uncomment this code when your getParseData function is finished
-  //This goes and gets new data every second, which mimicking a chat room experience.
-  // setInterval(function(){
-  //   $scope.getParseData();
-  // }, 1500)
+  //This goes and gets new data every second, which mimics a chat room experience.
+  setInterval(function(){
+    vm.getParseData();
+  }, 1500);
 })
--- js/parseService.js
+++ js/parseService.js
@@ -1,22 +1,68 @@
+/* global angular */
 var app = angular.module('chatroom');
 
-app.service('parseService', function($http){
+app.service('parseService', function($http, $q){
   //Here you'll need to create two methods. One called postData and the other called getData.
+  
+  // this.postData = function(message) {
+  //   var request = $http({
+  //     method: 'POST',
+  //     url: 'https://api.parse.com/1/classes/chat',
+  //     params: {
+  //       action: 'add'
+  //     },
+  //     data: {text: message}
+  //   });
+    
+  //   return (request.then(handleSuccess, handleFailure));
+  // };
+  
+  this.postData = function(message) {
+    var request = $http.post('https://api.parse.com/1/classes/chat', {text: message});
+    
+    return(request.then(handleSuccess, handleFailure));
+  }
+  
   
   //On the lines below create a getData method. This method will retrieve data from the parse backend.
   //The url for the get request should be 'https://api.parse.com/1/classes/chat?order=-createdAt'
   //Be sure to return whatever gets returned from $http so you can call .then in your controller.
- 
-
-
+  
+  
+  // this.getData = function() {
+  //   var request = $http({
+  //     method: 'GET',
+  //     params: {
+  //       action: 'get'
+  //     },
+  //     url: 'https://api.parse.com/1/classes/chat?order=-createdAt'
+  //   });
+    
+  //   return(request.then(handleSuccess, handleFailure));
+  // }
+  
+  
+  this.getData = function() {
+    var request = $http.get('https://api.parse.com/1/classes/chat?order=-createdAt');
+    
+    return(request.then(handleSuccess, handleFailure));
+  }
+  
+  
   //On the line below create the postData method. This method will add data to the parse backend.
   //The url for the request needs to be 'https://api.parse.com/1/classes/chat'
   //Because we're making a POST request, we need a way to tell parse the data we want to give it, 
     //in your $http call (along with url and method) have a data property which has a value that is equal to another object which a key of text and a value of the message being passed to parse. IE data: {text: yourMessage} 
   //Also, remember that $http returns a promise. So if you return the whole $http call (return $http(...)), you can then use .then in your controller.
+  function handleSuccess(response) {
+    return response.data;
+  }
   
-  //postData method here
-  
-  
-  //getData method here
+  function handleFailure(response) {
+    if (!angular.isObject(response.data) || !response.data.message) {
+      return $q.reject("An unknown error occurred");
+    }
+    
+    return ($q.reject(response.data.message)); 
+  }
 });
